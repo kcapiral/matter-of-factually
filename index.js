74a8cb27decd13e26f6a33b311b4d2af2fcbc6f0
Kathleen <kathleen.capiral@gmail.com>
2019-04-02 22:38:34 -0400

    edit 1

--- index.js
+++ index.js
@@ -5,10 +5,15 @@
 let pages = [];
 
 $(document).ready(() => {
+
   pages = $(".page").toArray();
   $(pages).hide();
+  $(".action-row").hide();
   firstPageFunction();
+
 });
+
+// $("#toNextPage(5)").window.open("https://www.somewebsite.com", "_blank", "toolbar=yes,top=500,left=500,width=400,height=400");
 
 //given the current page, go to current page + 1
 // aka, go to next page.
@@ -16,7 +21,7 @@ const toNextPage = (currentPage) => {
   // change header color:
   let nextPage = parseInt(currentPage) + 1;
   let pageContainerLeft = 100 * nextPage;
-  
+
   let headerColor = (nextPage % 2 != 0) ? "black" : "white";
   $("#header").css("color", headerColor);
 
@@ -36,14 +41,21 @@ const toNextPage = (currentPage) => {
     case 4:
       fifthPageFunction();
       break;
-    // case :
-    //   break;
+    case 5:
+        sixthPageFunction();
+      break;
+    case 6:
+        seventhPageFunction();
+      break;
+    case 7:
+        eigthPageFunction();
+      break;
     default:
       break;
   }
 
 
-  const pageContainer = $("#pages-container")
+const pageContainer = $("#pages-container")
     .css({"left": `-${pageContainerLeft}vw`});
 }
 
@@ -55,6 +67,7 @@ const firstPageFunction = () => {
   $(pages[0]).show();
   let secondary = $(".secondary");
   let secondaryItems = $(secondary).find("div");
+  console.log($(".action-row:eq(1)").show());
   let actionButton = $("#page-one-action");
   // console.log(actionButton);
   $(secondaryItems).css({"visibility": "hidden", "opacity": 0});
@@ -63,9 +76,9 @@ const firstPageFunction = () => {
 
 
   setTimeout(() => {
-    $("#test").fadeOut(1500);
+    $("#test").fadeOut(2000);
 
-    $(pages[0]).find(".initial").fadeOut(1500, () => {
+    $(pages[0]).find(".initial").fadeOut(1000, () => {
       secondary.show();
 
       // show first line
@@ -74,12 +87,12 @@ const firstPageFunction = () => {
       // show second line after 1500ms (1.5s)
       setTimeout(() => {
         $(secondaryItems[1]).css({"visibility": "visible", "opacity": 1});
-      }, 1500);
+      }, 1000);
 
       // show button after 2500ms (2.5s)
-      setTimeout(() => actionButton.fadeIn(1500), 2500);
+      setTimeout(() => actionButton.fadeIn(1000), 2500);
     });
-  }, 5000)
+  }, 2000)
 
 };
 
@@ -99,3 +112,20 @@ const fifthPageFunction = () => {
   $(".modal").hide();
   $(pages[4]).show();
 }
+
+const sixthPageFunction = () => {
+  $(pages[5]).show();
+}
+
+const seventhPageFunction = () => {
+  $(pages[6]).show();
+}
+
+const eigthPageFunction = () => {
+  $(pages[7]).show();
+}
+
+// function openWindow() {
+//   let facebookWindow = window.open('https://www.facebook.com', '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=500,width=400,height=400');
+//   $(".new-window");
+// }
