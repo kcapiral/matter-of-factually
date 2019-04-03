84e4feec103ed77dfdaa4c82280bfb1497414b95
Dan-Yoo <yoo.d93@gmail.com>
2019-04-02 23:23:10 -0400

    added blast js functionality

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
 // always start counting from 0 in programming.
 
 let pages = [];
+let buttons = [];
 
 $(document).ready(() => {
 
   pages = $(".page").toArray();
   $(pages).hide();
-  $(".action-row").hide();
+  buttons = $(".action-row");
+  buttons.hide();
   firstPageFunction();
 
 });
@@ -65,15 +67,16 @@ const openModal = () => {
 
 const firstPageFunction = () => {
   $(pages[0]).show();
+  $(buttons[0]).show();
   let secondary = $(".secondary");
   let secondaryItems = $(secondary).find("div");
-  console.log($(".action-row:eq(1)").show());
   let actionButton = $("#page-one-action");
-  // console.log(actionButton);
   $(secondaryItems).css({"visibility": "hidden", "opacity": 0});
   secondary.hide();
   actionButton.hide();
 
+  let firstWords = $("#page-0 .initial").blast({delimiter: "sentence"});
+  fadeInWords(firstWords, 1000);
 
   setTimeout(() => {
     $("#test").fadeOut(2000);
@@ -92,40 +95,62 @@ const firstPageFunction = () => {
       // show button after 2500ms (2.5s)
       setTimeout(() => actionButton.fadeIn(1000), 2500);
     });
-  }, 2000)
+  }, 6000)
 
 };
 
 const secondPageFunction = () => {
   $(pages[1]).show();
+  setTimeout(() => $(buttons[1]).fadeIn("slow"), 5000);
 }
 
 const thirdPageFunction = () => {
   $(pages[2]).show();
+  window.open("https://thehill.com/homenews/news/416255-black-security-guard-stops-shooter-then-is-shot-by-police-while-holding?fbclid=IwAR272RpyOKecaxjCi0ec0aIMm8q7bsoUl7EoeYnhjBqXM0hg7hcLvWfmyCY", "SingleSecondaryWindowName",
+      "resizable=yes,scrollbars=yes,status=yes");
+  setTimeout(() => $(buttons[2]).fadeIn("slow"), 5000);
 }
 
 const fourthPageFunction = () => {
   $(pages[3]).show();
+  // get the html with id="page-4" and get the child that has class="content"
+  let words = $("#page-4 .content").blast({delimiter: "word"});
+
+  //pass in the words, and pass in how much time between each word. 
+  fadeInWords(words, 700);
+
+  setTimeout(() => $(buttons[3]).fadeIn("slow"), 5000);
 }
 
 const fifthPageFunction = () => {
   $(".modal").hide();
   $(pages[4]).show();
+  setTimeout(() => $(buttons[4]).fadeIn("slow"), 5000);
 }
 
 const sixthPageFunction = () => {
   $(pages[5]).show();
+  setTimeout(() => $(buttons[5]).fadeIn("slow"), 5000);
 }
 
 const seventhPageFunction = () => {
   $(pages[6]).show();
+  setTimeout(() => $(buttons[6]).fadeIn("slow"), 5000);
+  window.open("https://www.wfaa.com/article/news/nba-will-consider-shortening-games-due-to-millennial-attention-spans/386064290", "SingleSecondaryWindowName",
+      "resizable=yes,scrollbars=yes,status=yes");
 }
 
 const eigthPageFunction = () => {
   $(pages[7]).show();
+  setTimeout(() => $(buttons[7]).fadeIn("slow"), 5000);
 }
 
-// function openWindow() {
-//   let facebookWindow = window.open('https://www.facebook.com', '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=500,width=400,height=400');
-//   $(".new-window");
-// }
+const fadeInWords = (words, increment) => {
+  $(words).hide();
+  let delay = 0;
+
+  for (let word of words) {
+    setTimeout(() => $(word).fadeIn(), (delay + increment));
+    delay += increment;
+  }
+}
